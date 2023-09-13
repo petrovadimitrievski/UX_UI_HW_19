ecc06e0b2c52e23c0a2af8d8e0d2ed5bc4ec011f
Zore <petrovazorka@yahoo.com>
2023-09-13 15:49:07 -0500

    add alert message
    I will delete it after Homework ( just for practice )

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 
+// I  will delete it after Homework ( this is just for practice ) 
+function stringConcat(){
+    var Welcome="Welcome and Thank you";
+    var Web=" for Visiting my Portfolio page";
+    var WT=Welcome+""+Web;
+    alert(WT);
+    console.log(WT);
+    
+    }
+    stringConcat();
+
+
 
 $(".toggle").click(function(){
     $(this).find(".toggleContainer").toggleClass("active") ;
 
 //  $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
 });
+
+
