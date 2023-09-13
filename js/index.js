b999beef0f2417bf2ba32bc191d2038a0ffeecaf
Zore <petrovazorka@yahoo.com>
2023-09-13 10:09:39 -0500

    start19

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+
+$(".toggle").click(function(){
+    $(this).find(".toggleContainer").toggleClass("active") ;
+
+//  $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+});
