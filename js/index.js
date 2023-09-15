4a1c33f488e70a9d257ae47854e562bf579ee6b6
Zore <petrovazorka@yahoo.com>
2023-09-14 22:01:33 -0500

    update code

--- js/index.js
+++ js/index.js
@@ -1,33 +1,26 @@
 
 // I  will delete it after Homework ( just for practice ) 
-function stringConcat(){
-    var Welcome="Welcome and Thank you";
-    var Web=" for Visiting my Portfolio page";
-    var WT=Welcome+""+Web;
-    alert(WT);
-    console.log(WT);
+// function stringConcat(){
+//     var Welcome="Welcome and Thank you";
+//     var Web=" for Visiting my Portfolio page";
+//     var WT=Welcome+""+Web;
+//     alert(WT);
+//     console.log(WT);
     
-    }
-    stringConcat();
+//     }
+//     stringConcat();
 
 
 
 $(".toggle").click(function(){
     $(this).find(".toggleContainer").toggleClass("active") ;
 
-//  $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
 });
 
 
 // This is also not working :( 
-// $(".topNav").css("color", "#ff0000");
+$("li").css("color", "#07439C");
 
 // This is unly working on My Projects-why?
 // $("li").css("color", "#ff0000");
 
-
-// write the text from Class vidio  (not working)
-// $ (".toggle").on("click", function()) {
-// $(".toggleContainer").toggle();
-
-// }
