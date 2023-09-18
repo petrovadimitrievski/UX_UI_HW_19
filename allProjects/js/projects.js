e1832d65567861f0099c06690dd3ca8318721577
Zore <petrovazorka@yahoo.com>
2023-09-18 11:11:59 -0500

    attach CV

---
+++ allProjects/js/projects.js
@@ -0,0 +1,10 @@
+
+function stringConcat(){
+    var Welcome="This page is under construction";
+    var Web=" Thank you for you patiance";
+    var WT=Welcome+""+Web;
+    alert(WT);
+    console.log(WT);
+    
+    }
+    stringConcat();
