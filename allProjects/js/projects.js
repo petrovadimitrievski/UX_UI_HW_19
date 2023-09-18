e5cd81c826c7363b6f787ff225e292c9c9941ac9
Zore <petrovazorka@yahoo.com>
2023-09-18 11:13:54 -0500

    gr

--- allProjects/js/projects.js
+++ allProjects/js/projects.js
@@ -1,7 +1,7 @@
 
 function stringConcat(){
     var Welcome="This page is under construction";
-    var Web=" Thank you for you patiance";
+    var Web="  Thank you for your patience";
     var WT=Welcome+""+Web;
     alert(WT);
     console.log(WT);
